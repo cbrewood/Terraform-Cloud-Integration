093768384df967701d8df8562a7ebb32d95c66e9
cbrewood <cb5869@att.com>
2021-02-17 15:53:56 +0000

    Added eu-west-2-tgw

--- main.tf
+++ main.tf
@@ -29,3 +29,38 @@ resource "aviatrix_vpc" "eu-west-2-vpc2" {
   aviatrix_transit_vpc = false
   aviatrix_firenet_vpc = false
 }
+
+resource "aviatrix_aws_tgw" "eu-west-2-tgw" {
+  account_name                      = "AWS_Automated"
+  attached_aviatrix_transit_gateway = [
+     "avx-transit-gw"
+  ]
+  aws_side_as_number                = "67000"
+  manage_vpc_attachment             = true
+  manage_transit_gateway_attachment = true
+  region                            = "eu-west-2"
+  tgw_name                          = "eu-west-2-tgw"
+
+  security_domains {
+    connected_domains    = [
+      "Default_Domain",
+      "Shared_Service_Domain"
+    ]
+    security_domain_name = "Aviatrix_Edge_Domain"
+  }
+
+  security_domains {
+    connected_domains    = [
+      "Aviatrix_Edge_Domain",
+      "Shared_Service_Domain"
+    ]    
+    security_domain_name = "Default_Domain"
+  }
+
+  security_domains {
+    connected_domains    = [
+      "Aviatrix_Edge_Domain",
+      "Default_Domain"
+    ]
+    security_domain_name = "Shared_Service_Domain"
+  }
