c90d71ebbf034e8cb0a9b41979935c48a2806228
cbrewood <cb5869@att.com>
2021-02-23 14:26:36 +0000

    100

--- main.tf
+++ main.tf
@@ -10,3 +10,56 @@ terraform {
 provider "aviatrix" {
 }
 
+resource "aviatrix_vpc" "eu-west-2-vpc1" {
+  cloud_type           = 1
+  account_name         = "AWS_Automated"
+  region               = "eu-west-2"
+  name                 = "eu-west-2-vpc1"
+  cidr                 = "10.100.0.0/16"
+  aviatrix_transit_vpc = false
+  aviatrix_firenet_vpc = false
+}
+
+resource "aviatrix_vpc" "eu-west-2-vpc2" {
+  cloud_type           = 1
+  account_name         = "AWS_Automated"
+  region               = "eu-west-2"
+  name                 = "eu-west-2-vpc2"
+  cidr                 = "10.101.0.0/16"
+  aviatrix_transit_vpc = false
+  aviatrix_firenet_vpc = false
+}
+
+resource "aviatrix_aws_tgw" "eu-west-2-tgw" {
+  account_name                      = "AWS_Automated"
+
+  aws_side_as_number                = "65000"
+  manage_vpc_attachment             = true
+  manage_transit_gateway_attachment = true
+  region                            = "eu-west-2"
+  tgw_name                          = "eu-west-2-tgw"
+
+  security_domains {
+    connected_domains    = [
+      "Default_Domain",
+      "Shared_Service_Domain"
+    ]
+    security_domain_name = "Aviatrix_Edge_Domain"
+  }
+
+  security_domains {
+    connected_domains    = [
+      "Aviatrix_Edge_Domain",
+      "Shared_Service_Domain"
+    ]    
+    security_domain_name = "Default_Domain"
+  }
+
+  security_domains {
+    connected_domains    = [
+      "Aviatrix_Edge_Domain",
+      "Default_Domain"
+    ]
+    security_domain_name = "Shared_Service_Domain"
+  }
+
